b80e81a996912c27256a850164f7968fab50ed22
toddbridges <tpbridges@mail.fhsu.edu>
2024-03-08 23:38:34 -0500

    3

--- api/quotes/read.php
+++ api/quotes/read.php
@@ -47,9 +47,9 @@ if($num > 0) {
     }
 
     //turn to json
-    // echo json_encode($quotes_arr);
-    $jsonArray = json_encode($quotes_arr);
-    return $jsonArray;
+    echo json_encode($quotes_arr);
+    //$jsonArray = json_encode($quotes_arr);
+    //return $jsonArray;
 } else {
     // no posts to show
     echo json_encode(
